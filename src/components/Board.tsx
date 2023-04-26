6223f0f73fd2126563041189cde4c36e740c0dfe
Daniel Krsticevic <danielkrsticevic@gmail.com>
2023-04-26 18:36:19 +0800

    making numbers fade when all occurances found

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -55,13 +55,16 @@ export function Board({newBoard}: BoardProps) {
             return
         }
         if (id === "Backspace"){
-            setBoard((board) => ({ ...board, [selectedCell]: 0}))
+            const tempBoard = [...board]
+            tempBoard[selectedCell] = 0
+            setBoard(tempBoard)
         }
         if (!keys.includes(id)){
             return
         }
-            setBoard((board) => ({ ...board, [selectedCell]: parseInt(id)}))
-        
+            const tempBoard = [...board]
+            tempBoard[selectedCell] = parseInt(id)
+            setBoard(tempBoard) 
     }
 
     useEffect(() => {
@@ -124,7 +127,7 @@ export function Board({newBoard}: BoardProps) {
 
     function checkWin() {
         let temp: string = JSON.stringify(board)
-        if (!temp.replaceAll("0\":","").includes("0")){
+        if (!temp.includes("0")){
             setSelectedCell(-1)
             setSelectedCol(-1)
             setSelectedGroup(-1)
@@ -135,7 +138,7 @@ export function Board({newBoard}: BoardProps) {
 
     return(
             
-            <Card style={{marginTop: "20px", display: "flex", justifyContent: "center", alignItems: "center", minWidth: "400px", width: "650px", aspectRatio: "8/11"}}>
+            <Card style={{marginTop: "20px", display: "flex", justifyContent: "center", alignItems: "center", minWidth: "400px", width: "650px", aspectRatio: "7/9"}}>
             <Card.Title style={{height: "30px",}}>
                 {won ? "Board Complete!" : ""}
             </Card.Title>
@@ -149,7 +152,7 @@ export function Board({newBoard}: BoardProps) {
                 )}
             </Row> )}
             <Keyboard board={board} keyPressed={handleKeyboard}></Keyboard>
-            <Button style={{marginTop: "2%"}} onClick={(e) => {won ? undefined : setBoard(newBoard); e.currentTarget.blur()}}>
+            <Button style={{marginTop: "2%", marginBottom: "2%"}} onClick={(e) => {won ? undefined : setBoard(newBoard); e.currentTarget.blur()}}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
                 <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
